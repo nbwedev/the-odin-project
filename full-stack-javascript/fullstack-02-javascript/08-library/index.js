96101521e0078ec7e488ea881d3feb1fedd2337d
Neil Benolirao <neilskillsets.initiative@gmail.com>
2025-09-20 17:59:40 +0800

    feat: add constructor,helper functions

--- full-stack-javascript/fullstack-02-javascript/08-library/index.js
+++ full-stack-javascript/fullstack-02-javascript/08-library/index.js
@@ -1,9 +1,26 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+// Book constructor
+function Book(title, author, page, read) {
+  this.title = title;
+  this.author = author;
+  this.page = page;
+  this.read = read;
+
+  this.info = function () {
+    const readStatus = this.read ? "read" : "not read yet";
+    return `${this.title} by ${this.author}, ${page} pages, ${readStatus}`;
+  };
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+// Take params, create a book then store it in the array
+function addBookToLibrary(title, author, page, read) {
+  const book = new Book(title, author, page, read);
+  myLibrary.push(book);
 }
+
+// // test
+// addBookToLibrary("12", 12, 123, false);
+// addBookToLibrary("12", 12, 123, true);
+
+// console.log(myLibrary);
